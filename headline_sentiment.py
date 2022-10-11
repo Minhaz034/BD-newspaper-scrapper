e36667b67450252c044c88922683dc30e78a9051
shadman-shahid <shadman9085@gmail.com>
2022-10-11 09:06:23 +0600

    Updated sentiment analysis pipeline.

--- headline_sentiment.py
+++ headline_sentiment.py
@@ -4,6 +4,7 @@ from transformers import AutoTokenizer
 from transformers import AutoModel
 from transformers import AutoModelForSequenceClassification
 import torch
+import os
 import time
 
 sample_raw_inputs=["Agricultural machinery is beyond the reach of farmers",
@@ -11,11 +12,30 @@ sample_raw_inputs=["Agricultural machinery is beyond the reach of farmers",
             "Job opportunities in ACI Group with attractive salary"]
 model_path ="./bertweet-base-sentiment-analysis"
 
+
 def get_sentiment(raw_inputs, model_path ="./bertweet-base-sentiment-analysis" ):
     print(f"{torch.cuda.device_count()} GPU available")
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
-    model = AutoModelForSequenceClassification.from_pretrained(model_path).to(device)
+    try:
+        if os.path.exists(model_path):
+            print("Model already on device!")
+            tokenizer = AutoTokenizer.from_pretrained(model_path)
+            model = AutoModelForSequenceClassification.from_pretrained(model_path).to(device)
+        else:
+            tokenizer = AutoTokenizer.from_pretrained("finiteautomata/bertweet-base-sentiment-analysis")
+            model = AutoModelForSequenceClassification.from_pretrained("finiteautomata/bertweet-base-sentiment-analysis").to(device)
+
+            tokenizer.save_pretrained(model_path)
+            model.save_pretrained(model_path)
+
+    except:
+        tokenizer = AutoTokenizer.from_pretrained("finiteautomata/bertweet-base-sentiment-analysis")
+        model = AutoModelForSequenceClassification.from_pretrained(
+            "finiteautomata/bertweet-base-sentiment-analysis").to(device)
+
+        tokenizer.save_pretrained(model_path)
+        model.save_pretrained(model_path)
+
     encoded_inputs = tokenizer(raw_inputs, padding=True, truncation=True, return_tensors="pt").to(device)
     model_outputs = model(**encoded_inputs)
     predictions = torch.nn.functional.softmax(model_outputs['logits'], dim=-1)
@@ -27,7 +47,7 @@ def get_sentiment(raw_inputs, model_path ="./bertweet-base-sentiment-analysis" )
         print('Memory Usage:')
         print('Allocated:', round(torch.cuda.memory_allocated(0) / 1024 ** 3, 1), 'GB')
         print('Cached:   ', round(torch.cuda.memory_reserved(0) / 1024 ** 3, 1), 'GB')
-    return predictions,sentiments
+    return predictions, sentiments
 
 
 if __name__ == '__main__':
